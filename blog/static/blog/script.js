77217b9837f20eb18ff72db02cdc7c09c9bcef4e
Олександр Січ <s.sich@ukr.net>
2024-03-29 14:28:21 +0200

    add slider

---
+++ blog/static/blog/script.js
@@ -0,0 +1,21 @@
+
+function slidePrev()
+{
+    let activeSlide = document.querySelector('.slide.active');
+    let toSlide = activeSlide.previousElementSibling;
+    if (!toSlide)
+        toSlide = activeSlide.parentElement.lastElementChild;
+    activeSlide.classList.remove('active');
+    toSlide.classList.add('active');
+}
+
+function slideNext()
+{
+    let activeSlide = document.querySelector('.slide.active');
+    let toSlide = activeSlide.nextElementSibling;
+    toSlide = activeSlide.nextElementSibling;
+        if (!toSlide)
+            toSlide = activeSlide.parentElement.firstElementChild;
+    activeSlide.classList.remove('active');
+    toSlide.classList.add('active');
+}

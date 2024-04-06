1f7690ea2dfa080a8fad39a2068afc2dacd17206
Oleksandr Sich <s.sich@ukr.net>
2024-04-06 10:56:06 +0300

    prepare pub

--- homework_site/settings.py
+++ homework_site/settings.py
@@ -51,10 +51,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # my
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    
-    
+# my 
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+        
+# 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -63,6 +64,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 ROOT_URLCONF = 'homework_site.urls'
 

d9a9b6779a4ff164cae96ce6e642bb4dbc4087d0
Oleksandr Sich <s.sich@ukr.net>
2024-04-06 10:08:59 +0300

    prepare pub

--- homework_site/settings.py
+++ homework_site/settings.py
@@ -51,7 +51,10 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    # my
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    
+    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',

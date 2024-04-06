7b99eee79276e02dd4af13c9f8774a1e429dc153
Oleksandr Sich <s.sich@ukr.net>
2024-04-06 08:33:15 +0300

    prepare pub

--- homework_site/settings.py
+++ homework_site/settings.py
@@ -128,6 +128,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Define the directory where static files will be collected
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 

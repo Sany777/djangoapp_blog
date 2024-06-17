918cd520a82465bb2909b7695fa15c08934d9f87
Олександр Січ <s.sich@ukr.net>
2024-06-17 16:07:50 +0300

    mend

--- staticfiles/blog/js/redirect_script.203e668a5c5d.js
+++
@@ -1,9 +0,0 @@
-const DELAY_MS = 2500;
-
-const REDIRECT_URL = '/blog/';
-
-function redirectToDashboard() {
-    window.location.href = REDIRECT_URL;
-}
-
-setTimeout(redirectToDashboard, DELAY_MS);

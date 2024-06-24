07163037c1db854cb28ea6a6c1f250ee288ce36e
Олександр Січ <s.sich@ukr.net>
2024-06-24 12:27:57 +0300

    fix redirect behavoiur

--- portal/static/portal/js/edit_script.js
+++ portal/static/portal/js/edit_script.js
@@ -1,12 +1,13 @@
 const textarea = document.getElementById('auto-resize');
 
-textarea.addEventListener('scroll', () => {
+
+textarea?.addEventListener('scroll', () => {
     textarea.style.height = textarea.scrollHeight+40 + 'px'; 
 });
 
 (()=>{
     if( !document.getElementById('edit-flag')){
-        textarea.disabled = true;
+        textarea?.disabled = true;
     }
 })()
 

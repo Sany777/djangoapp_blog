aca37e1b29dec9dfc391387bd32fb0b2147e6f0e
Олександр Січ <s.sich@ukr.net>
2024-06-25 11:13:36 +0300

    fix loading static files

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from .models import *
 
+from django.contrib.sessions.models import Session
+
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ['session_key', 'session_data', 'expire_date']
+
+admin.site.register(Session, SessionAdmin)
 
 admin.site.register(Topic)
 admin.site.register(Entry)
